39990bf281e340962462e0c50318047b29469709
97254 <97254@MATAN>
2022-11-14 18:01:39 +0200

    testing two finger detection

--- App1/StockChart.cs
+++ App1/StockChart.cs
@@ -50,7 +50,24 @@ namespace App1
                 if (heighest == 0) {findLowHeigh();}
                 CreatChartPoints();
                 DrawPoints();
+                DrawTouching();
                 Invalidate();
+            }
+        }
+
+        Paint p1 = new Paint();
+       
+
+        MyPoint point1;
+        MyPoint point2;
+
+        private void DrawTouching()
+        {
+            p1.Color = Color.Black;
+            if(point1 != null && point2 != null)
+            {
+                canvas.DrawCircle(point1.x, point1.y, 100, p1);
+                canvas.DrawCircle(point2.x, point2.y, 100, p1);
             }
         }
 
@@ -88,41 +105,38 @@ namespace App1
             }
         }
 
+
+        int counter = 0;
         public override bool OnTouchEvent(MotionEvent e)
         {
-            if(lastPlace == null)
+            if (e.PointerCount > 1)
+            {
+                p1.Color = Color.Black;
+
+                point1 = new MyPoint((float)e.GetX(), (float)e.GetY());
+                point2 = new MyPoint(e.GetAxisValue(Axis.X, e.FindPointerIndex(e.GetPointerId(1))), e.GetAxisValue(Axis.Y, e.FindPointerIndex(e.GetPointerId(1))));
+
+                Console.WriteLine("touching screen" + counter);
+                counter++;
+
+                Invalidate();
+            }
+            if(e.Action == MotionEventActions.Up)
+            {
+                point1 = null;
+                point2 = null;
+            }
+
+            if (lastPlace == null)
             {
                 lastPlace = new MyPoint(e.GetX(), e.GetY());
                 return true;
             }
             else
             {
-                if (e.PointerCount > 1)
+                if (e.Action == MotionEventActions.Move)
                 {
-                    Paint p1 = new Paint();
-                    p1.Color = Color.Black;
-
-                    //e.GetToolMajor(e.FindPointerIndex(1));
-                    //test_zoomfactor += (e.GetToolMajor(e.GetPointerId(0)) - lastPlace.x) / 100000;
-
-                    MyPoint point1 = new MyPoint((float)e.GetX(), (float)e.GetY());
-                   
-
-
-
-                    //MyPoint point2 = new MyPoint(e.GetAxisValue(Axis.X), e.GetAxisValue(Axis.Y));
-                    MyPoint point2 = new MyPoint(e.GetAxisValue(Axis.X, e.FindPointerIndex(e.GetPointerId(1))), e.GetAxisValue(Axis.Y, e.FindPointerIndex(e.GetPointerId(1))));
-
-
-                    canvas.DrawCircle(point1.x, point1.y, 100, p1);
-                    canvas.DrawCircle(point2.x, point2.y, 100, p1);
-                    Invalidate();
-
-                    //test_zoomfactor += ((float)e.GetX() - lastPlace.x) / 100;
-                }
-                if (e.Action == MotionEventActions.Move )
-                {
-                    if(e.PointerCount > 1)
+                    if (e.PointerCount > 1)
                     {
 
                     }
@@ -141,9 +155,71 @@ namespace App1
                 }
             }
 
-            lastPlace = new MyPoint(e.GetX(), e.GetY());
+            lastPlace.x = e.GetX();
+            lastPlace.y = e.GetY();
             return true;
         }
 
+            
     }
+
+        //public override bool OnTouchEvent(MotionEvent e)
+        //{
+        //    if(lastPlace == null)
+        //    {
+        //        lastPlace = new MyPoint(e.GetX(), e.GetY());
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        if (e.PointerCount > 1)
+        //        {
+        //            Paint p1 = new Paint();
+        //            p1.Color = Color.Black;
+
+        //            //e.GetToolMajor(e.FindPointerIndex(1));
+        //            //test_zoomfactor += (e.GetToolMajor(e.GetPointerId(0)) - lastPlace.x) / 100000;
+
+        //            MyPoint point1 = new MyPoint((float)e.GetX(), (float)e.GetY());
+
+
+
+
+        //            //MyPoint point2 = new MyPoint(e.GetAxisValue(Axis.X), e.GetAxisValue(Axis.Y));
+        //            MyPoint point2 = new MyPoint(e.GetAxisValue(Axis.X, e.FindPointerIndex(e.GetPointerId(1))), e.GetAxisValue(Axis.Y, e.FindPointerIndex(e.GetPointerId(1))));
+
+
+        //            canvas.DrawCircle(point1.x, point1.y, 100, p1);
+        //            canvas.DrawCircle(point2.x, point2.y, 100, p1);
+
+        //            Invalidate();
+        //            //test_zoomfactor += ((float)e.GetX() - lastPlace.x) / 100;
+        //        }
+        //        if (e.Action == MotionEventActions.Move )
+        //        {
+        //            if(e.PointerCount > 1)
+        //            {
+
+        //            }
+        //            else
+        //            {
+        //                if (Zoom)
+        //                {
+        //                    test_zoomfactor += ((float)e.GetX() - lastPlace.x) / 100;
+        //                }
+        //                if (Move)
+        //                {
+        //                    camera.CameraOffSetX += (float)e.GetX() - lastPlace.x;
+        //                    camera.CameraOffSetY += (float)e.GetY() - lastPlace.y;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    lastPlace = new MyPoint(e.GetX(), e.GetY());
+
+        //    return true;
+        //}
+
+    //}
 }
