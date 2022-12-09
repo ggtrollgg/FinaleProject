3ea5de15b6bdb8c4415c3491611a519b00eb4908
97254 <97254@MATAN>
2022-12-09 16:33:47 +0200

    buttons in home screen

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -19,7 +19,7 @@ namespace App1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btnstart,btnToActivity2;
+        Button btnstart,btnToListView,btnExit;
         
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -29,14 +29,27 @@ namespace App1
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
             btnstart = FindViewById<Button>(Resource.Id.btnstart);
+            btnExit = FindViewById<Button>(Resource.Id.btnExit);
+            btnToListView = FindViewById<Button>(Resource.Id.btnToListView);
             btnstart.Click += Btnstart_Click;
-            
+            btnExit.Click += BtnExit_Click;
+            btnToListView.Click += BtnToListView_Click;
             //btnToActivity2 = FindViewById<Button>(Resource.Id.btnToActivity2);
             //btnToActivity2.Click += BtnToActivity2_Click;
 
 
         }
 
+        private void BtnToListView_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(StockViewActivity));
+            StartActivity(intent);
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Finish();
+        }
 
         private void Btnstart_Click(object sender, EventArgs e)
         {
