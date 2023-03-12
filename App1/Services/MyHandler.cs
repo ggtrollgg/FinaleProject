3adaa3b1d0ba794f1f585a3b7283a07d7aa66092
97254 <97254@MATAN>
2023-03-12 20:19:21 +0200

    useless service :(

---
+++ App1/Services/MyHandler.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App1
+{
+    internal class MyHandler : Handler
+    {
+        Context context;
+
+
+        [Obsolete]
+        public MyHandler(Context context)
+        {
+            this.context = context;
+
+        }
+        [Obsolete]
+        public override void HandleMessage(Message msg)
+        {
+            Toast.MakeText(context, "" + msg.Arg1, ToastLength.Short).Show();
+        }
+    }
+}
